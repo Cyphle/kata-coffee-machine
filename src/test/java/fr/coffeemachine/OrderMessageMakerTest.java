8627996d3f788a7dad4a0f59015c021a9794c8c9
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-09-07 16:01:37 +0200

    Build message for a chocolate with 2 sugars

--- src/test/java/fr/coffeemachine/OrderMessageMakerTest.java
+++ src/test/java/fr/coffeemachine/OrderMessageMakerTest.java
@@ -1,5 +1,6 @@
 package fr.coffeemachine;
 
+import fr.coffeemachine.drinks.Chocolate;
 import fr.coffeemachine.drinks.Coffee;
 import fr.coffeemachine.drinks.Drink;
 import org.junit.Before;
@@ -27,5 +28,12 @@ public class OrderMessageMakerTest {
     Drink coffeeWithOneSugar = new Coffee();
     coffeeWithOneSugar.addSugar(1);
     assertThat(messageMaker.makeMessageForOrderOf(coffeeWithOneSugar)).isEqualTo(new OrderMessage("Drink maker makes 1 coffee with 1 sugar and a stick"));
+  }
+
+  @Test
+  public void should_make_message_for_a_chocolate_with_two_sugars_and_a_stick() throws Exception {
+    Drink chocolateWithTwoSugars = new Chocolate();
+    chocolateWithTwoSugars.addSugar(2);
+    assertThat(messageMaker.makeMessageForOrderOf(chocolateWithTwoSugars)).isEqualTo(new OrderMessage("Drink maker makes 1 chocolate with 2 sugars and a stick"));
   }
 }
